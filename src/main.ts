060d8aafa612242d9b09f44f79298862fa4d5ec7
MohamedSalah54 <mohamed.salah199988.com>
2026-01-26 15:06:14 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,35 @@
+// import { ValidationPipe } from '@nestjs/common';
+// import cookieParser from 'cookie-parser';
+// import { AppModule } from './app.module';
+// import { NestFactory } from '@nestjs/core';
+// import { queryParser } from './common/middlewares/qs.middleware';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   app.use(cookieParser());
+
+//   app.use(queryParser);
+
+//   app.useGlobalPipes(
+//     new ValidationPipe({
+//       whitelist: true,
+//       forbidNonWhitelisted: true,
+//       transform: true,
+//     }),
+//   );
+
+//   app.enableCors({
+//     origin: ['http://localhost:3000', 'https://treva-platform.vercel.app'],
+//     credentials: true,
+//   });
+
+//   const port = process.env.PORT || 3001;
+
+//   await app.listen(port);
+// }
+// bootstrap();
+
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
@@ -7,7 +39,19 @@ import { queryParser } from './common/middlewares/qs.middleware';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://treva-platform.vercel.app'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+
   app.use(cookieParser());
+  app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') return res.sendStatus(204);
+    next();
+  });
 
   app.use(queryParser);
 
@@ -19,13 +63,7 @@ async function bootstrap() {
     }),
   );
 
-  app.enableCors({
-    origin: ['http://localhost:3000', 'https://treva-platform.vercel.app'],
-    credentials: true,
-  });
-
   const port = process.env.PORT || 3001;
-
   await app.listen(port);
 }
 bootstrap();
